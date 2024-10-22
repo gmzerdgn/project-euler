314b6d4fb62a5e07fdc3a3371a63f7887cc5020c
= <=>
2024-10-22 14:19:28 +0300

    5

--- 3 largest prime number.py
+++ 3 largest prime number.py
@@ -1,23 +1,56 @@
-# Largest Prime Factor
+import math
+import time
 
-def prime_number(b):
-    c1 = 0
-    for i in range(1,int(b/2)+1):
-        if b % i == 0:
-            c1 += 1
-    if c1 < 2:
-        return(True)
+def divides(n, i):
+    int(n)
+    int(i)
+    if n%i == 0:
+        returnValue = True
     else:
-        return(False)
+        returnValue = False
+    return returnValue
 
-def prime_factor(a):
-    n = 0
-    count = 1
-    prime = 0
-    while n < a+1:
-        n += 1
-        if a % n == 0:
-            if prime_number(n) == True:
-                prime = n
-                print(prime)
-print(prime_factor(600851475143))
+def isPrime(number, primes):
+    for prime in primes:
+        if number%prime == 0:
+            return False
+    return True
+
+n = 600851475143
+n = 10000000
+
+start = time.time()
+print(start)
+primesList = [2]
+for i in (range(3,n+1,2)):
+    if isPrime(i, primesList):
+        primesList.append(i)
+
+end = time.time()
+print(end - start)
+print(primesList)
+
+# # Largest Prime Factor
+
+# def prime_number(b):
+#     c1 = 0
+#     for i in range(1,int(b/2)+1):
+#         if b % i == 0:
+#             c1 += 1
+#     if c1 < 2:
+#         return(True)
+#     else:
+#         return(False)
+
+# def prime_factor(a):
+#     n = 0
+#     count = 1
+#     prime = 0
+#     while n < a+1:
+#         n += 1
+#         if a % n == 0:
+#             if prime_number(n) == True:
+#                 prime = n
+#                 print(prime)
+# prime_factor(600851475143)
+
--- 5 smallest multiple.py
+++ 5 smallest multiple.py
@@ -1,1 +1,43 @@
-smallest multiple
+# 2520$ is the smallest number that can be divided by each of the numbers from $1$ to $10$ without any remainder.</p>
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 - 20
+
+# totalDivisors = []
+# primeList = []
+# def findDivisors(n):
+# def findPrimes(n):
+    
+
+# def prime_number(b):
+#     c1 = 0
+#     for i in range(1,int(b/2)+1):
+#         if b % i == 0:
+#             c1 += 1
+#     if c1 < 2:
+#         return(True)
+#     else:
+#         return(False)
+
+# def prime_factor(a):
+#     n = 0
+#     count = 1
+#     prime = 0
+#     while n < a+1:
+#         n += 1
+#         if a % n == 0:
+#             if prime_number(n) == True:
+#                 prime = n
+#                 print(prime)
+
+totalDivisors = []
+total = 1
+for i in range(2,21):
+    for divisor in totalDivisors:
+        if i%divisor == 0:
+            i = i / divisor
+    i = int(i)
+    totalDivisors.append(i)
+    total = total * i
+    
+print(totalDivisors)
+print(total)
+
